8a0756ac1b2070195aa942bb83acbe88000974cd
AzureDoom <7415711+AzureDoom@users.noreply.github.com>
2026-04-23 08:00:05 -0400

    Forgot include with command

---
+++ src/main/java/com/azuredoom/tagcore/data/TranslationKeys.java
@@ -0,0 +1,8 @@
+package com.azuredoom.tagcore.data;
+
+public class TranslationKeys {
+
+    public static final String RELOAD_SUCCESS = "server.msg.tagcore.success";
+
+    public static final String RELOAD_FAILED = "server.msg.tagcore.failed";
+}
